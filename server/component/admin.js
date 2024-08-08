943f2eedc0b940a615d9165909d4fc96c745d786
robennals <rob@ennals.org>
2024-08-08 16:28:45 -0700

    FIX: admin.org should be admin when running under emulation

--- server/component/admin.js
+++ server/component/admin.js
@@ -4,7 +4,9 @@ async function getIsUserAdminAsync({siloKey, userEmail}) {
     const adminEmails = await firebaseReadAsync(['silo', siloKey, 'module-public', 'admin', 'adminEmails']);
     const emailDomain = userEmail?.split('@')[1];
     const isTest = process.env.NODE_ENV == 'test';
-    if (adminEmails?.includes(userEmail) || (isTest && emailDomain == 'admin.org')) {
+    const isEmulator = process.env.FUNCTIONS_EMULATOR === 'true';
+
+    if (adminEmails?.includes(userEmail) || ((isTest || isEmulator) && emailDomain == 'admin.org')) {
         return true;
     } else {
         return false;
