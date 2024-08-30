dd8b0dbf2b68a92e99dcfab5d96b1595e78ef89b
robennals <rob@ennals.org>
2024-08-30 13:59:17 -0700

    Don't sent notif email if user has email notifs disabled

--- server/component/__tests__/notifs.test.js
+++ server/component/__tests__/notifs.test.js
@@ -1,27 +1,31 @@
 const { sendTemplatedEmailAsync } = require("../../util/email");
 const { mockServerStore } = require("../../util/serverstore");
-const { setTestData } = require("../../util/testutil");
 const { sendNotifsForReplyApi } = require("../notifs");
-
-// const sendEmail = jest.fn();
 
 jest.mock('../../util/email', () => ({sendTemplatedEmailAsync: jest.fn()}));
 
-test('sendNotifsForReplyApi', async () => {
-    setTestData({
-        silo: {testSilo: {
-            'module-public': {admin: {name: 'Radio Canada'}},
-            structure: {testStruct: {instance: {testInstance: { 
-                global: {name: 'My Conversation'},
-                collection: {comment: {
-                    '1': {text: 'Hello', from: 'testuser'},
-                    '2': {text: 'My reply', from: 'testuser', replyTo: '1'}
-                }}
-            }}}}
-        }}
-    })
-
+test('noNotifIfDisabled', async () => {
     const serverstore = mockServerStore();
+    serverstore.setRemoteGlobal({
+        structureKey: 'profile', 
+        instanceKey: 'testuser', 
+        key: 'fields', value: {emailNotifsDisabled: true}
+    });
+    serverstore.setObject('comment', '1', {text: 'Hello', from: 'testuser'});
+    serverstore.setObject('comment', '2', {text: 'My reply', from: 'testuser', replyTo: '1'});
+    await serverstore.commitDataAsync();
+
+    await sendNotifsForReplyApi({serverstore, parentKey: '1', replyKey: '2', language: 'English'});
+    expect(sendTemplatedEmailAsync).not.toHaveBeenCalled();
+})
+
+test('sendNotifsForReplyApi', async () => {
+    const serverstore = mockServerStore();
+    serverstore.setModulePublic('admin', 'name', 'Radio Canada');
+    serverstore.setGlobalProperty('name', 'My Conversation');
+    serverstore.setObject('comment', '1', {text: 'Hello', from: 'testuser'});
+    serverstore.setObject('comment', '2', {text: 'My reply', from: 'testuser', replyTo: '1'});
+    await serverstore.commitDataAsync();
 
     const result = await sendNotifsForReplyApi({serverstore, parentKey: '1', replyKey: '2', language: 'English'});
     expect(sendTemplatedEmailAsync).toHaveBeenCalledWith({
--- server/component/notifs.js
+++ server/component/notifs.js
@@ -9,9 +9,21 @@ async function sendNotifsForReplyApi({serverstore, language, parentKey, replyKey
     const parentComment = await pParentComment;
     const replyComment = await pReplyComment;
     const conversationName = await pConversationName;
+
+    if (!parentComment || !replyComment) {
+        throw new Error('Parent or reply comment not found:' + parentKey + ' ' + replyKey);
+    }
+
     const siloName = await pSiloName ?? serverstore.getSiloKey().toUpperCase();
     const replyAuthorPersona = await serverstore.getPersonaAsync(replyComment.from);
-    const toPersona = await serverstore.getPersonaAsync(parentComment.from);
+    const toPersona = await serverstore.getPersonaAsync(parentComment?.from);
+    const toFields = await serverstore.getRemoteGlobalAsync({
+        structureKey: 'profile', instanceKey: parentComment?.from, key: 'fields'
+    });
+
+    if (toFields?.emailNotifsDisabled) {
+        return;
+    }
 
     await sendTemplatedEmailAsync({
         templateId: 'reply-notif', language, 
