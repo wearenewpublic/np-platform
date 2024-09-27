ce51b89eac85defc0471f556d78673d4ebcf70e7
robennals <rob@ennals.org>
2024-09-27 15:23:29 -0700

    FIX: Byline should be one line in "small" mode

--- client/component/people.js
+++ client/component/people.js
@@ -2,7 +2,7 @@ import { useDatastore, useIsLive, useObject, usePersonaKey, usePersonaObject, us
 import { Image, StyleSheet, Text, View } from "react-native";
 import { IconCircleCheck } from "./icon";
 import { UtilityText } from "./text";
-import { HoverView, Pad, spacings } from "./basics";
+import { HoverView, Pad, PadBox, spacings } from "./basics";
 import { formatDate, formatMiniDate } from "./date";
 import { colorBlack, colorDisabledBackground, colorTextGrey, colorWhite, colorPink } from "./color";
 import { TextButton } from "./button";
@@ -38,7 +38,7 @@ function renderLayers(type, persona) {
     const { profilePhotoLayers } = useConfig();
     const size = sizeMap[type] ?? 32;
     
-    return <view style={{
+    return <View style={{
                 position: 'absolute',
                 left: 0,
                 top: 0,
@@ -47,7 +47,7 @@ function renderLayers(type, persona) {
                 backgroundColor: 'transparent'
     }}>
         {profilePhotoLayers && profilePhotoLayers.map((item,i) => item(persona))}
-    </view>
+    </View>
 }
 
 export function MyProfilePhoto({type='large', photo=null, faint=false, check=false, border=false}) {
@@ -132,37 +132,58 @@ const FacePileStyle = StyleSheet.create({
     }
 })
 
+function TagRow({tags}) {
+    const s = TagRowStyle;
+    return <View style={s.outer}>
+        {tags.map((tag, i) => <View key={i} style={s.tag}>
+            {tag}
+        </View>)}
+    </View>
+}
+const TagRowStyle = StyleSheet.create({
+    outer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    tag: {
+        marginRight: 4,
+    }
+});
+
 export function Byline({ type = 'small', photoType = null, clickable = true, userId, name = null, photo = null, time, subtitleLabel, subtitleParams = {}, underline = false, edited = false }) {
 
-    const { bylineTag } = useConfig();
+    const { bylineTags } = useConfig();
     const s = BylineStyle;
     const persona = usePersonaObject(userId);
     const language = useLanguage();
     const datastore = useDatastore();
-    const tag = (bylineTag && bylineTag(persona)) || null;
+    const tags = bylineTags?.map(tagFunc => tagFunc(persona)).filter(tag => tag);
+    const hasTags = tags?.length > 0;
 
     function onProfile() {
         datastore.gotoInstance({ structureKey: 'profile', instanceKey: userId });
     }
-            return <View style={s.outer}>
-                <ProfilePhoto userId={userId} photo={photo} type={photoType ?? type} />
-                <Pad size={spacings.xs} />
-                <View style={{ flexDirection: 'column' }}>
-                    <View style={(tag) ? {flexDirection: 'row' } : {flexDirection: 'column' } }>
-                                {clickable ?
-                                <><TextButton type='small' strong text={name ?? persona?.name} underline={underline} onPress={onProfile} /></>
-                                :
-                                <><UtilityText strong text={name ?? persona?.name} underline={underline} /></>
-                        }
-                        {time && <><Pad size={(tag) ? spacings.xs : 0} /><UtilityText color={colorTextGrey}
-                            label={'{time}' + (edited ? ' • Edited' : '')}
-                            formatParams={(type == 'small') ? { time: formatMiniDate(time, language) } : { time: formatDate(time, language) } } underline={underline} /></>
-                        }
-                    </View>
-                    {tag}
-                </View>
+    const oneLine = hasTags || type == 'small';
+    return <View style={s.outer}>
+        <ProfilePhoto userId={userId} photo={photo} type={hasTags ? 'large' : photoType ?? type} />
+        <Pad size={spacings.xs} />
+        <View style={{ flexDirection: 'column' }}>
+            <View style={oneLine ? {flexDirection: 'row' } : {flexDirection: 'column'}}>
+                {clickable ?
+                    <TextButton type='small' strong text={name ?? persona?.name} underline={underline} onPress={onProfile} />
+                :
+                    <UtilityText strong text={name ?? persona?.name} underline={underline} />
+                }
+                <Pad size={oneLine ? 8 : 4} />
+                {time && <UtilityText color={colorTextGrey}
+                    label={'{time}' + (edited ? (' • ' + (oneLine ? 'edited' : 'Edited')) : '')}
+                    formatParams={oneLine ? { time: formatMiniDate(time, language) } : { time: formatDate(time, language) } } underline={underline} />
+                }
             </View>
-    }
+            {hasTags && <PadBox top={4}><TagRow tags={tags} /></PadBox>}
+        </View>
+    </View>
+}
 
 
 const BylineStyle = StyleSheet.create({
