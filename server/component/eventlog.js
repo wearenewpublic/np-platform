b6acf4f21ebe42bc8fd5972e60d31eaccffb4d81
robennals <rob@ennals.org>
2024-11-22 16:16:06 -0800

    Log aggregation and retention

--- server/component/eventlog.js
+++ server/component/eventlog.js
@@ -1,5 +1,5 @@
 const { checkIsGlobalAdmin } = require("../util/admin");
-const { firebaseWriteAsync, firebaseUpdateAsync, createNewKey, firebaseGetUserAsync, firebaseReadAsync, firebaseReadWithFilterAsync } = require("../util/firebaseutil");
+const { firebaseWriteAsync, firebaseUpdateAsync, createNewKey, firebaseGetUserAsync, firebaseReadAsync, firebaseReadWithFilterAsync, firebaseReadPaginatedAsync } = require("../util/firebaseutil");
 
 async function logEventApi({
         sessionKey, isNewSession, deviceInfo=null, userId, 
@@ -88,14 +88,83 @@ async function getSingleSessionApi({serverstore, sessionKey}) {
 exports.getSingleSessionApi = getSingleSessionApi;
 
 
+function getDayNumberForTime(time) {
+    const millisecondsPerDay = 24 * 60 * 60 * 1000;
+    return Math.floor(time / millisecondsPerDay);
+}
+
+function getIsoDateForTime(time) {
+    return new Date(time).toISOString().substring(0, 10);
+}
+
+function updateEventCountsForEvent({eventCounts, event}) {
+    if (!eventCounts[event.eventType]) {
+        eventCounts[event.eventType] = {};
+    }
+    const day = new Date(event.time).toISOString().substring(0, 10);
+    eventCounts[event.eventType][day] = (eventCounts[event.eventType][day] || 0) + 1;
+}
+
+async function aggregateLogsAsync({serverstore, stride=1000} = {}) {
+    const daysToKeep = 30;
+    const thisDay = getDayNumberForTime(Date.now());
+    const oldestDayToKeep = thisDay - daysToKeep;
+    var startAfter = null;
+    var eventCounts = {};
+    var eventsToDelete = {};
+    const limit = stride;
+    var events = null;
+    var badEvents = {};
+
+    do {
+        events = await firebaseReadPaginatedAsync(['log', 'event'], limit, startAfter);
+        if (!events) break;
+        const eventKeys = Object.keys(events);
+        for (const eventKey of eventKeys) {
+            const event = events[eventKey];
+            try {
+                if (getDayNumberForTime(event.time) < oldestDayToKeep) {
+                    updateEventCountsForEvent({eventCounts, event});
+                    eventsToDelete[eventKey] = null;
+                } else if (event.time && event.eventType) {
+                    // TOO New to delete, but valid
+                } else {
+                    console.error('bad event', event);
+                    badEvents[eventKey] = event;
+                    eventsToDelete[eventKey] = null;
+                }
+            } catch (e) {
+                badEvents[eventKey] = event;
+                eventsToDelete[eventKey] = null;
+                console.error('Bad event', event, e);
+            }
+        }
+        startAfter = eventKeys[eventKeys.length - 1];
+    } while (events && Object.keys(events).length >= limit);
+
+    const types = Object.keys(eventCounts);
+    for (const type of types) {
+        for (const day of Object.keys(eventCounts[type])) {
+            serverstore.doDelayedWrite(['log', 'aggregated', type, day], eventCounts[type][day]);
+        }
+    }
+    serverstore.doDelayedWrite(['log', 'badEvents'], badEvents);
+
+    for (const key of Object.keys(eventsToDelete)) {
+        serverstore.doDelayedWrite(['log', 'event', key], null);
+    }
+}
+exports.aggregateLogsAsync = aggregateLogsAsync;
+
+
 exports.publicFunctions = {
     logEvent: logEventApi,
     setSessionUser: setSessionUserApi,
+    aggregateLogs: aggregateLogsAsync,
 }
 
 exports.adminFunctions = {
     getEvents: getEventsApi,
     getSessions: getSessionsApi,
-    getSingleSession: getSingleSessionApi
+    getSingleSession: getSingleSessionApi,
 }
-
